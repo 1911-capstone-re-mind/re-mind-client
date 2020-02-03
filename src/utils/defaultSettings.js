adf9013d74fc232c4cef052cf8edca86fb61cfe9
Marius Maries <mariuscm@gmail.com>
2020-02-03 17:09:23 -0600

    temp testing timings

--- src/utils/defaultSettings.js
+++ src/utils/defaultSettings.js
@@ -2,13 +2,14 @@
 module.exports = {
   posture: {
     duration: 5000,
-    frequency: 30 * 60000
+    frequency: 15000
+    // 30 * 60000
   },
   movement: {
     duration: 5 * 60000,
     frequency: 60 * 60000
   },
-  eyeStrain: {
+  vision: {
     duration: 20000,
     frequency: 20 * 60000
   },
@@ -17,8 +18,9 @@ module.exports = {
     frequency: 60 * 60000
   },
   mindfulness: {
-    duration: 10 * 60000,
-    frequency: 240 * 60000
+    duration: 15000,//10 * 60000,
+    frequency: 5000
+    //240 * 60000
   }
 }
 
