8fdb18ce83e9233790e81b1075b2b0b980b90504
Vishal Patel <vpatel1215@gmail.com>
2020-02-05 16:26:14 -0600

    finally got daily chart view working properly

--- src/components/DailyDashboard.js
+++ src/components/DailyDashboard.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import { VictoryChart, VictoryBar, VictoryPie } from "victory"
-import { sendSettingsToMain, sendDelayToMain } from '../dataToMainProcess'
+import React from "react";
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryPie,
+  VictoryAxis,
+  VictoryTheme
+} from "victory";
+import { sendSettingsToMain, sendDelayToMain } from "../dataToMainProcess";
+import { connect } from "react-redux";
+import { fetchLog } from "../store/reducers/activityLogReducer";
 
 class DailyDashboard extends React.Component {
   constructor(props) {
@@ -9,48 +17,93 @@ class DailyDashboard extends React.Component {
       data: {}
     };
   }
+  componentDidMount() {
+    try {
+      this.props.fetchLog(this.props.user.id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   handleSendSettings() {
-    sendSettingsToMain('settings')
+    sendSettingsToMain("settings");
   }
 
   handleSendDelay() {
-    sendDelayToMain('delay')
+    sendDelayToMain("delay");
   }
 
   render() {
+    // let sessArr = this.props.activityLog.map(log => {
+    //   if (log.date === 2) {
+    //     return log.completed_sessions;
+    //   }
+    // });
+    // sessArr = sessArr.filter(function(element) {
+    //   return element !== undefined;
+    // });
+    const log = this.props.activityLog;
+    // log.filter(d => {
+    //   if (d.date === 3) {
+    //     return d;
+    //   }
+    // });
+    console.log("props", this.props);
     return (
       <div className="daily-dashboard">
-        <div className="daily-dashboard-header">
-          Your daily report
-        </div>
+        <div className="daily-dashboard-header">Your daily report</div>
         <button onClick={this.handleSendSettings}>go</button>
         <button onClick={this.handleSendDelay}>go</button>
-        <VictoryChart
-        domainPadding={20}
-        >
-        <VictoryBar
-        style={{ data: { fill: "blue" } }}
-        data={[
-          { x: 1, y: 2 },
-          { x: 2, y: 3 },
-          { x: 3, y: 5 },
-          { x: 4, y: 4 },
-          { x: 5, y: 6 }
-        ]}
-        />
+        <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+          <VictoryAxis
+            label="Activity"
+            tickValues={[1, 2, 3, 4, 5]}
+            tickFormat={[
+              "Posture",
+              "Movement",
+              "Eye Strain",
+              "Hydration",
+              "Mindfulness"
+            ]}
+          />
+          <VictoryAxis
+            dependentAxis
+            label="Sessions"
+            // tickFormat specifies how ticks should be displayed
+            tickFormat={t => `${t}`}
+          />
+          <VictoryBar
+            style={{ data: { fill: "blue" } }}
+            data={log.filter(d => d.date === 3)}
+            // data accessor for x values
+            x="userPreferenceId"
+            // data accessor for y values
+            y="completed_sessions"
+          />
         </VictoryChart>
         <VictoryPie
-        colorScale={["orange", "cyan"]}
-        data={[
-          { x: " 80% completed", y: 80 },
-          { x: "20% ignored", y: 20 }
-        ]}
-        width={300}
+          colorScale={["orange", "cyan"]}
+          data={[
+            { x: " 80% completed", y: 80 },
+            { x: "20% ignored", y: 20 }
+          ]}
+          width={300}
         />
       </div>
     );
   }
 }
 
-export default DailyDashboard;
+const mapState = state => {
+  return {
+    user: state.user,
+    activityLog: state.activityLog
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    fetchLog: userId => dispatch(fetchLog(userId))
+  };
+};
+export default connect(mapState, mapDispatch)(DailyDashboard);
