6161c5850d0f2c6836bf714e92f4d7cbd3bca172
rtakahashi <lou.takahashi@gmail.com>
2020-02-06 15:59:41 -0600

    cleaned up Hello component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 class Hello extends React.Component {
@@ -8,4 +7,4 @@ class Hello extends React.Component {
   }
 }
 
-export default connect(null, mapDispatch)(Hello);
+export default Hello;
