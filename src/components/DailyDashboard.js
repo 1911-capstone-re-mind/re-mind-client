068cee33de00cd16e085a84dd99b429c50797b49
Vishal Patel <vpatel1215@gmail.com>
2020-02-06 11:35:46 -0600

    got pie chart reflecting data without minutes label

--- src/components/DailyDashboard.js
+++ src/components/DailyDashboard.js
@@ -36,7 +36,17 @@ class DailyDashboard extends React.Component {
   render() {
     const log = this.props.activityLog;
 
-    console.log("props", log);
+    console.log(
+      "props",
+      log
+        .filter(d => d.date === 3)
+        .filter(
+          x =>
+            (x.userPreferenceId === 2) +
+            (x.userPreferenceId === 3) +
+            (x.userPreferenceId === 5)
+        )
+    );
     return (
       <div className="daily-dashboard">
         <div className="daily-dashboard-header">Your daily report</div>
@@ -71,11 +81,20 @@ class DailyDashboard extends React.Component {
         </VictoryChart>
         <VictoryPie
           colorScale={["orange", "cyan", "red"]}
-          data={[
-            { x: " 80 minutes", y: 30 },
-            { x: "20 minutes", y: 20 },
-            { x: "50 minutes", y: 50 }
-          ]}
+          tickValues={[1, 2, 3]}
+          tickFormat={["Movement", "Eye Strain", "Mindfulness"]}
+          data={log
+            .filter(d => d.date === 3)
+            .filter(
+              x =>
+                (x.userPreferenceId === 2) +
+                (x.userPreferenceId === 3) +
+                (x.userPreferenceId === 5)
+            )}
+          // data accessor for x values
+          x="user_preference.activity.name"
+          // data accessor for y values
+          y={d => (d.completed_sessions * d.user_preference.duration) / 60000}
           width={500}
         />
       </div>
