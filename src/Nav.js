f5ec7042846666fa93910c8830cd9f6f2562c3be
Vishal Patel <vpatel1215@gmail.com>
2020-02-06 16:50:40 -0600

    logout is now working

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "./store/reducers/userReducer";
 
-function Nav() {
-  return (
-    <div className='nav'>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/test">test</Link>
-    </div>
-  );
+class Nav extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/test">test</Link>
+        <a href="#" onClick={this.handleClick}>
+          Logout
+        </a>
+      </div>
+    );
+  }
 }
-
-export default Nav;
+const mapDispatch = dispatch => {
+  return {
+    logout: () => dispatch(logout())
+  };
+};
+export default connect(null, mapDispatch)(Nav);
